c016d0d081cf21d7c459b6419539e9d36fe0be58
vikram823 <deshmukhvikram927@gmail.com>
2021-10-01 12:51:41 +0530

    add rermove friend

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchUserProfile } from "../actions/profile";
+import { APIs } from "../helpers/url";
+import { getAuthTokenFromLocalStorage } from "../helpers/utils";
+import { addFriend, removeFriend } from "../actions/friends";
 
 class UserProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      success: null,
+      error: null,
+      successMessage: null,
+    };
+  }
+
   componentDidMount() {
     const { match } = this.props;
 
@@ -10,6 +22,80 @@ class UserProfile extends Component {
       this.props.dispatch(fetchUserProfile(match.params.userId));
     }
   }
+
+  chechIfFriendIsFriend = () => {
+    const { match, friends } = this.props;
+    const userId = match.params.userId;
+
+    const index = friends.map((friend) => friend.to_user._id).indexOf(userId);
+
+    if (index !== -1) {
+      return true;
+    }
+
+    return false;
+  };
+
+  handleAddFriendClick = async () => {
+    const userId = this.props.match.params.userId;
+    const url = APIs.addFriend(userId);
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${getAuthTokenFromLocalStorage()}`,
+      },
+    };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log("DATA", data);
+    if (data.success) {
+      this.setState({
+        success: true,
+        successMessage: "Added Friend Successfully!",
+      });
+
+      this.props.dispatch(addFriend(data.data.friendship));
+    } else {
+      this.setState({
+        success: null,
+        error: data.message,
+      });
+    }
+  };
+
+  handleRemoveFriendClick = async () => {
+    const userId = this.props.match.params.userId;
+    const url = APIs.removeFriend(userId);
+
+    const extra = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${getAuthTokenFromLocalStorage()}`,
+      },
+    };
+
+    const response = await fetch(url, extra);
+    const data = await response.json();
+    console.log("await data", data);
+
+    if (data.success) {
+      this.setState({
+        success: true,
+        successMessage: "Removed Friend Successfully!",
+      });
+
+      this.props.dispatch(removeFriend(userId));
+    } else {
+      this.setState({
+        success: null,
+        error: data.message,
+      });
+    }
+  };
 
   render() {
     const { profile } = this.props;
@@ -19,6 +105,8 @@ class UserProfile extends Component {
       return <h1>LOADING...</h1>;
     }
 
+    const isAFriend = this.chechIfFriendIsFriend();
+    const { success, error, successMessage } = this.state;
     return (
       <div className="settings">
         <div className="img-container">
@@ -39,16 +127,36 @@ class UserProfile extends Component {
         </div>
 
         <div className="btn-grp">
-          <button className="button save-btn">Add Friend</button>
+          {!isAFriend ? (
+            <button
+              className="button save-btn"
+              onClick={this.handleAddFriendClick}
+            >
+              Add Friend
+            </button>
+          ) : (
+            <button
+              className="button save-btn"
+              onClick={this.handleRemoveFriendClick}
+            >
+              Remove Friend
+            </button>
+          )}
+
+          {success && (
+            <div className="alert success-dialog">{successMessage}</div>
+          )}
+          {error && <div className="alert error-dialog"> {error}</div>}
         </div>
       </div>
     );
   }
 }
 
-function mapStateToProps({ profile }) {
+function mapStateToProps({ profile, friends }) {
   return {
     profile,
+    friends,
   };
 }
 
